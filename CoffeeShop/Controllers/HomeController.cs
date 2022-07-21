85a1c4452661f2db9dd347927526ffac85738208
VinhDang88 <vincedang720@gmail.com>
2022-07-21 17:09:56 -0400

    First commit

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult UserCheck()
+        {
+            return View();
+        }
+
+        public IActionResult UserCheckResult(User x)
+        {
+            return View(x);
+            Console.WriteLine("Welcome!");
+        }
+
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
